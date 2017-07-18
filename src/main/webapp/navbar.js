10b3c924c797e7b63467427a5aa7a7a6d1d475f2
Tiezzynator <tiezzynator@gmail.com>
2017-07-18 11:40:37 +0200

    pls_fix commit

--- src/main/webapp/navbar.js
+++ src/main/webapp/navbar.js
@@ -1,9 +1,18 @@
 $(".dropdown-content").hide();
 
 $(".dropbtn").mouseenter(
-    function() {
+    function () {
         $(".dropdown-content").fadeIn();
     });
-$(".dropdown-content").mouseleave(function(){
+$(".dropdown-content").mouseleave(function () {
     $(".dropdown-content").fadeOut();
+
+});
+
+$(".search").keypress( function (e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        $("#snackbar").empty().append("This function is still WIP");
+        $("#snackbar").fadeIn(400).delay(1500).fadeOut(500);
+    }
 });
